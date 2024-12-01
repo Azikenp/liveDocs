f003271fc8441492ed4af95512896b6e98214b9a
azikenp <azikenp26@gmail.com>
2024-12-01 14:38:05 +0100

    new files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
-import "./globals.css"
+import "./globals.css";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
   children,
@@ -15,16 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          ...
-        </body>
-      </head>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
