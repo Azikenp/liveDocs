6ca3bcd009a3a4b1c1403e2090445c83ce14ae3e
azikenp <azikenp26@gmail.com>
2024-12-08 09:23:03 +0100

    add doc

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,36 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Home = () => {
+  const documents = [];
+
   return (
     <main className="home-container">
       <Header className="sticky left-4 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
-          Notifications
+          Notificationsss
           <SignedIn>
             <UserButton />
           </SignedIn>
         </div>
       </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="empty doc"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentBtn />
+        </div>
+      )}
     </main>
   );
 };
