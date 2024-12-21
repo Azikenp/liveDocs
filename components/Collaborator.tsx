3a39e5a70421d9d9965c6c1a8ad10a27d813958a
azikenp <azikenp26@gmail.com>
2024-12-21 21:34:18 +0100

    collaborator image

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Collaborator = ({
@@ -13,7 +14,19 @@ const Collaborator = ({
   const shareDocumentHandler = async (type: string) => {};
   const removeCollaboratorHandler = async (email: string) => {};
 
-  return <div>Collaborator</div>;
+  return (
+    <li className="flex items-center justify-between gap-2 py-3">
+      <div className="flex gap-2">
+        <Image
+          src={collaborator.avatar}
+          width={36}
+          height={36}
+          className="size-none rounded-full"
+          alt="avatar"
+        />
+      </div>
+    </li>
+  );
 };
 
 export default Collaborator;
