07b8da552c39d72b42dc5805ad9487a4b18dc133
azikenp <azikenp26@gmail.com>
2024-12-22 19:19:39 +0100

    delete button

--- lib/actions/room-actions.ts
+++ lib/actions/room-actions.ts
@@ -136,3 +136,13 @@ export const removeCollaborator = async ({
     console.log(`Error happened while removing a collaborator ${error}`);
   }
 };
+
+export const deleteDocument = async(roomId: string) => {
+  try {
+    await liveblocks.deleteRoom(roomId)
+    revalidatePath('/');
+    redirect('/')
+  } catch (error) {
+    console.log(`Error happened while deleting a room ${error}`);
+  }
+}
