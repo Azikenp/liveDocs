118c3faaf04394b55c5fb811951499396e2e89f8
azikenp <azikenp26@gmail.com>
2024-12-24 21:10:52 +0100

    addditions to deletemodal

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -58,8 +58,7 @@ export const DeleteModal = ({ roomId }: DeleteModalProps) => {
           />
           <DialogTitle>Delete document</DialogTitle>
           <DialogDescription>
-            Are you sure you want to delete this document? This action cannot be
-            undone.
+            Are you sure you want to delete this document? This action is permananet and cannot be undone.
           </DialogDescription>
         </DialogHeader>
 
